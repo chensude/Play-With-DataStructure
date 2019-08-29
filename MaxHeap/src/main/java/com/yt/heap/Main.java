3fa9f64298edb0d68aa9b1518d64b8a74ffb0cb7
chensude <18112925131@163.com>
2019-08-29 15:20:31 +0800

    add HeapMax

---
+++ MaxHeap/src/main/java/com/yt/heap/Main.java
@@ -0,0 +1,26 @@
+package com.yt.heap;
+
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int n = 100;
+
+        MaxHeap<Integer> maxHeap = new MaxHeap<>();
+        Random random = new Random();
+        for(int i = 0 ; i < n ; i ++)
+            maxHeap.add(random.nextInt(Integer.MAX_VALUE));
+
+        int[] arr = new int[n];
+        for(int i = 0 ; i < n ; i ++)
+            arr[i] = maxHeap.extractMax();
+
+        for(int i = 1 ; i < n ; i ++)
+            if(arr[i-1] < arr[i])
+                throw new IllegalArgumentException("Error");
+
+        System.out.println("Test MaxHeap completed.");
+    }
+}
---
+++ MaxHeap/src/main/java/com/yt/heap/MaxHeap.java
@@ -0,0 +1,75 @@
+package com.yt.heap;
+
+public class MaxHeap<E extends Comparable<E>> {
+
+    private Array<E> data;
+
+    public MaxHeap(int capacity) {
+        data= new Array(capacity);
+    }
+
+    public MaxHeap() {
+        this(10);
+    }
+
+    public int getSize() {
+
+        return data.getSize();
+    }
+
+    public boolean isEmpty() {
+        return data.isEmpty();
+    }
+
+    public int parent(int index) {
+        if(index==0) {
+            throw new IllegalArgumentException("index0-don't have parent");
+        }
+        return (index-1)/2;
+    }
+
+    public int leftParent(int index) {
+        return 2*index+1;
+    }
+
+    public int rightParent(int index) {
+        return 2*index+2;
+    }
+
+
+    public void add (E e) {
+        data.addLast(e);
+        shiftUp(data.getSize()-1);
+    }
+
+    private void shiftUp(int k) {
+        while (k>0&&data.get(parent(k)).compareTo(data.get(k))<0) {
+            data.swap(parent(k),k);
+            k=parent(k);
+        }
+    }
+
+    public E extractMax() {
+        E e = data.get(0);
+
+        data.swap(0,data.getSize()-1);
+        data.removeLast();
+        shiftDown(0);
+        return e;
+    }
+
+    private void shiftDown(int k) {
+        while (leftParent(k)<data.getSize()) {
+            int j = leftParent(k);
+            if(j+1<data.getSize()&&data.get(j+1).compareTo(data.get(j))>0) {
+                j = rightParent(k);
+            }
+            if(data.get(k).compareTo(data.get(j)) >= 0 ) {
+                break;
+            }
+                data.swap(k,j);
+                k=j;
+        }
+
+    }
+}
